2bd6427e491a0a4e3b38c27fd0d6146a7d5e8fe1
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2023-09-10 00:25:19 +0200

    inital

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
 # Define the repository URL
-REPO_URL="https://github.com/exactlyprateek/react_blogs.git"
+REPO_URL="https://github.com/exactlyprateek/statoc-react-app.git"
 
 # Update package list and install curl
 sudo apt update && sudo apt install -y curl
 
+sudo apt install nginx
 # Install Node.js
-curl -fsSL https://deb.nodesource.com/setup_14.x | sudo bash -
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo bash -
 sudo apt-get install -y nodejs
 
 # Clone the repository
 git clone $REPO_URL
 
 # Navigate to the project directory (assuming repository name is "project-name")
-cd project-name
+cd statoc-react-app
 
 # Install node dependencies
 npm install
 
 # Run the app in production mode
-npm start
+npm run build
+
+mv build /var/www/html
+
+cd build
+
+find . -maxdepth 1 -exec mv {} .. \;
+
+sudo systemctl restart nginx
